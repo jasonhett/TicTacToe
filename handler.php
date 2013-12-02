8add776e32034f5c0fc2c51512ed171d488c7f14
Jason Hettmansperger <jjbeanz@Jasons-MacBook-Pro-2.local>
2013-12-02 15:28:21 -0500

    changes

--- handler.php
+++ handler.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: jjbeanz
- * Date: 12/1/13
- * Time: 3:25 PM
- * To change this template use File | Settings | File Templates.
- */
+
 
 define( 'JOINS_CHECK_INTERVAL', 1000000 );
 define( 'GAMES_CHECK_INTERVAL', 100000 );
